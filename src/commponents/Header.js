ca1f3ae23148fce80f3de207bcc9cc879d19129b
fmfahath <fmfahath.dev@gmail.com>
2024-02-04 10:03:27 +0530

    header section : obile view added

--- src/commponents/Header.js
+++ src/commponents/Header.js
@@ -1,13 +1,30 @@
+import { Bars3Icon } from '@heroicons/react/24/solid'
+
+
 export default function Header() {
     return (
-        <header className="flex justify-between px-5 py-3 text-xl bg-primary">
+        <header className="flex justify-between px-3 py-3 text-xl bg-primary">
             <a href="#" className="text-white font-bold text-3xl">CODE-FM</a>
-            <ul className="flex text-white">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Peojects</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
+            <nav className="hidden md:block">
+                <ul className="flex text-white">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Peojects</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+
+            {/* Moble view nav */}
+            <nav className="block md:hidden ">
+                <ul className="flex flex-col text-white mobile-nav">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Peojects</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <button className='block md:hidden'><Bars3Icon className='text-white h-7'></Bars3Icon></button>
         </header>
     )
 }
+
