d3ea330692f2d596dc9caf945f148aecd4bc3dd5
fmfahath <fmfahath.dev@gmail.com>
2024-02-04 10:07:13 +0530

    header section : togglemenu conditon added

--- src/commponents/Header.js
+++ src/commponents/Header.js
@@ -1,7 +1,10 @@
 import { Bars3Icon } from '@heroicons/react/24/solid'
+import { useState } from 'react'
 
 
 export default function Header() {
+    const [toggleBtn, setToggleBtn] = useState(false);
+
     return (
         <header className="flex justify-between px-3 py-3 text-xl bg-primary">
             <a href="#" className="text-white font-bold text-3xl">CODE-FM</a>
@@ -15,15 +18,15 @@ export default function Header() {
             </nav>
 
             {/* Moble view nav */}
-            <nav className="block md:hidden ">
+            {toggleBtn && <nav className="block md:hidden ">
                 <ul className="flex flex-col text-white mobile-nav">
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Peojects</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
-            </nav>
-            <button className='block md:hidden'><Bars3Icon className='text-white h-7'></Bars3Icon></button>
+            </nav>}
+            <button onClick={() => setToggleBtn(!toggleBtn)} className='block md:hidden'><Bars3Icon className='text-white h-7'></Bars3Icon></button>
         </header>
     )
 }
