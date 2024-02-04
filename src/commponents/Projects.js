d400fc3841e5358a73c492446dac8487a36f21a0
fmfahath <fmfahath.dev@gmail.com>
2024-02-04 14:56:07 +0530

    projects section : project card hover layer / effect added

--- src/commponents/Projects.js
+++ src/commponents/Projects.js
@@ -4,19 +4,33 @@ import projectImg3 from '../assets/proj-3.png';
 
 export default function Projects() {
     return (
-        <section className='px-5 py-10 bg-secondary'>
+        <section className='flex flex-col justify-center items-center px-5 py-10 bg-secondary'>
             <div className='flex justify-center mb-10'>
                 <h1 className='text-white text-4xl font border-b-[2px] tracking-wide'> Recent Projects </h1>
             </div>
-            <div className='flex flex-row justify-center gap-5'>
-                <div className='p-2 bg-white rounded-xl'>
+            <div className='flex flex-row justify-center gap-5 w-3/4 '>
+                <div className='project-card'>
                     <img src={projectImg1} className='rounded-xl'></img>
+                    <div className='project-card-layer hover:opacity-100'>
+                        <p>E-Commerce - Online Shopping Site with Admin Dash Board</p>
+                        <button className='project-view-btn'>View</button>
+                    </div>
                 </div>
-                <div className='p-2 bg-white rounded-xl'>
+                <div className='project-card'>
                     <img src={projectImg2} className='rounded-xl'></img>
+                    <div className='project-card-layer hover:opacity-100'>
+                        <p>Creative Agency - Digital Designing Website using React JS Technology</p>
+                        <button className='project-view-btn'>View</button>
+                    </div>
                 </div>
-                <div className='p-2 bg-white rounded-xl'>
+                <div className='project-card'>
                     <img src={projectImg3} className='rounded-xl'></img>
+                    <div className='project-card-layer hover:opacity-100' >
+                        <p>Web Development Site Portfolio with Responsive Design . Build
+                            Basic on Bootstrap Web Template
+                        </p>
+                        <button className='project-view-btn'>View</button>
+                    </div>
                 </div>
             </div>
         </section>
